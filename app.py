2ac0cc80a5ba2272bd95c251ec8829ba1319ad53
felipe <felipe@aaaaaaaaaaaaaaaaaaaa.com>
2024-04-18 23:13:59 -0300

    passando bastao

--- app.py
+++ app.py
@@ -5,8 +5,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello marinheiro!'
+    global bola
+    bola += 1
+    return f'valor de bola: {bola}'
+
+
+@app.route('/interact')
+def interact():  # put application's code here
+    with open("bola.txt", "r") as file:
+        file.write("0")
 
 
 if __name__ == '__main__':
     app.run()
+    print("chegou???")
---
+++ board.md
@@ -0,0 +1,12 @@
+## ROADMAP
+
+# focar API
+- endpoint que recebe como parametro:
+- o nome do arquivo (sessao) - obrigatorio -> apenda
+- valor a ser apended - opcional -> retorna o arquivo
+
+
+
+# exemplo
+
+
